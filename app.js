983694df28fd80f0a294bfa7f3e1a0eada2527be
Mohamad Hasyim Ridwan <123049383+mhrdwan@users.noreply.github.com>
2024-01-08 19:22:45 +0700

    tesst ridwan

--- app.js
+++ app.js
@@ -32,7 +32,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
 app.use('/assets', express.static('assets'));
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/guest', GuestRouter);
